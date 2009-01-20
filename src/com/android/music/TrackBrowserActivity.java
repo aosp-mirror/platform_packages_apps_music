b3f1e54c6c3dd6b61df23fcbda8915eb51027769
The Android Open Source Project <initial-contribution@android.com>
2009-01-20 14:04:00 -0800

    auto import from //branches/cupcake/...@127101

--- src/com/android/music/TrackBrowserActivity.java
+++ src/com/android/music/TrackBrowserActivity.java
@@ -782,7 +782,7 @@ public class TrackBrowserActivity extends ListActivity
          */
         super.onCreateOptionsMenu(menu);
         if (mPlaylist == null) {
-            menu.add(0, PLAY_ALL, 0, R.string.play_all).setIcon(R.drawable.ic_menu_play_clip);
+            menu.add(0, PLAY_ALL, 0, R.string.play_all).setIcon(com.android.internal.R.drawable.ic_menu_play_clip);
         }
         menu.add(0, GOTO_START, 0, R.string.goto_start).setIcon(R.drawable.ic_menu_music_library);
         menu.add(0, GOTO_PLAYBACK, 0, R.string.goto_playback).setIcon(R.drawable.ic_menu_playback)
@@ -791,7 +791,7 @@ public class TrackBrowserActivity extends ListActivity
         if (mPlaylist != null) {
             menu.add(0, SAVE_AS_PLAYLIST, 0, R.string.save_as_playlist).setIcon(android.R.drawable.ic_menu_save);
             if (mPlaylist.equals("nowplaying")) {
-                menu.add(0, CLEAR_PLAYLIST, 0, R.string.clear_playlist).setIcon(R.drawable.ic_menu_clear_playlist);
+                menu.add(0, CLEAR_PLAYLIST, 0, R.string.clear_playlist).setIcon(com.android.internal.R.drawable.ic_menu_clear_playlist);
             }
         }
         return true;
