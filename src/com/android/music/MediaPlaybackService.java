1b8cc16760222cc4f2b85a42697d92615b68a3e5
Thomas Tuttle <thinkinginbinary@gmail.com>
2009-01-28 21:06:46 -0500

    Make play() call next() if we are at end of track (and not REPEAT_CURRENT).

--- src/com/android/music/MediaPlaybackService.java
+++ src/com/android/music/MediaPlaybackService.java
@@ -905,6 +905,12 @@ public class MediaPlaybackService extends Service {
      */
     public void play() {
         if (mPlayer.isInitialized()) {
+            // if we are at the end of the song, go to the next song first
+            if (mRepeatMode != REPEAT_CURRENT &&
+                mPlayer.position() >= mPlayer.duration() - 1) {
+                next(true);
+            }
+
             mPlayer.start();
             setForeground(true);
             mWasPlaying = true;
