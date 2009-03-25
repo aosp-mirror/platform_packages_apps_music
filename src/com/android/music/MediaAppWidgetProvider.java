349c020e7f44674c305f3035275a79cde987c605
Jeffrey Sharkey <>
2009-03-24 21:27:02 -0700

    Automated import from //branches/donutburger/...@142100,142100

--- src/com/android/music/MediaAppWidgetProvider.java
+++ src/com/android/music/MediaAppWidgetProvider.java
@@ -71,6 +71,7 @@ public class MediaAppWidgetProvider extends AppWidgetProvider {
         final RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.album_appwidget);
         
         views.setTextViewText(R.id.title, res.getText(R.string.emptyplaylist));
+        views.setTextViewText(R.id.artist, null);
 
         linkButtons(context, views, false /* not playing */);
         pushUpdate(context, appWidgetIds, views);
@@ -116,30 +117,32 @@ public class MediaAppWidgetProvider extends AppWidgetProvider {
         final RemoteViews views = new RemoteViews(service.getPackageName(), R.layout.album_appwidget);
         
         final int track = service.getQueuePosition() + 1;
-        final String titleName = service.getTrackName();
-        final String artistName = service.getArtistName();
+        CharSequence titleName = service.getTrackName();
+        CharSequence artistName = service.getArtistName();
         
         // Format title string with track number, or show SD card message
-        CharSequence titleString = "";
         String status = Environment.getExternalStorageState();
-        if (titleName != null) {
-            titleString = res.getString(R.string.gadget_track_num_title, track, titleName);
-        } else if (status.equals(Environment.MEDIA_SHARED) ||
+        if (status.equals(Environment.MEDIA_SHARED) ||
                 status.equals(Environment.MEDIA_UNMOUNTED)) {
-            titleString = res.getText(R.string.sdcard_busy_title);
+            titleName = res.getText(R.string.sdcard_busy_title);
         } else if (status.equals(Environment.MEDIA_REMOVED)) {
-            titleString = res.getText(R.string.sdcard_missing_title);
-        } else {
-            titleString = res.getText(R.string.emptyplaylist);
+            titleName = res.getText(R.string.sdcard_missing_title);
+        } else if (titleName == null) {
+            titleName = res.getText(R.string.emptyplaylist);
         }
         
-        views.setTextViewText(R.id.title, titleString);
+        views.setTextViewText(R.id.title, titleName);
         views.setTextViewText(R.id.artist, artistName);
         
         // Set correct drawable for pause state
         final boolean playing = service.isPlaying();
-        views.setImageViewResource(R.id.control_play, playing ?
-                R.drawable.appwidget_pause : R.drawable.appwidget_play);
+        if (playing) {
+            views.setImageViewResource(R.id.control_play, R.drawable.appwidget_pause);
+            views.setImageViewResource(R.id.led, R.drawable.appwidget_led_anim);
+        } else {
+            views.setImageViewResource(R.id.control_play, R.drawable.appwidget_play);
+            views.setImageViewResource(R.id.led, R.drawable.appwidget_led0);
+        }
 
         // Link actions buttons to intents
         linkButtons(service, views, playing);
