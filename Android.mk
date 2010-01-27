ff3d4370df4f501863e0491d04170d4bbdd60005
Marco Nelissen <marcone@google.com>
2010-01-27 09:46:40 -0800

    One step closer to unbundling. Build against the current SDK, and use fully qualified java package names in the manifest.

--- Android.mk
+++ Android.mk
@@ -8,6 +8,8 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src) \
 
 LOCAL_PACKAGE_NAME := Music
 
+LOCAL_SDK_VERSION := current
+
 include $(BUILD_PACKAGE)
 
 # Use the folloing include to make our test apk.
